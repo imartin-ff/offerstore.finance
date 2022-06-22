e0e9a43a088f65ee1110b0fe6d58a5f6890c7949
MaRTiN <maxfedorenko20023106@gmail.com>
2022-06-22 16:35:13 +0300

    final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
 document.querySelector('.logo').addEventListener('click', function(){
     document.querySelector('.page-container').classList.toggle('dark-theme');
 })
+
+document.querySelector('.burger-menu').addEventListener('click', function(){
+    this.classList.toggle('opened');
+    document.querySelector('.header-btns').classList.toggle('show');
+})
+
+const swiper = new Swiper('.reviews-slider-right', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    wrapperClass: 'reviews-slider-wrapper',
+    slideClass: 'reviews-slider-slide',
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        reverseDirection: false
+    },
+
+    
+})
+
+
+const swiper2 = new Swiper('.reviews-slider-left', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    wrapperClass: 'reviews-slider-wrapper',
+    slideClass: 'reviews-slider-slide',
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        reverseDirection: true
+    },
+})
